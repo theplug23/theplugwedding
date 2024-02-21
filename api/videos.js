261da26d971f58fd40c0849b7b3a498b43b42605
theplug23 <travauxtheplug@gmail.com>
2024-02-21 10:06:51 +0100

    ajout des images de video

--- api/videos.js
+++ api/videos.js
@@ -2,13 +2,16 @@ import vid1 from '/public/images/videos/Alice&Dirk.jpg'
 import vid2 from '/public/images/videos/Philippe&Marie.jpeg'
 import vid3 from '/public/images/videos/Maxime&Maria.jpeg'
 import vid4 from '/public/images/videos/Julia&Eric.jpg'
+import vid5 from '/public/images/videos/Lisa&Charles.png'
+import vid6 from '/public/images/videos/vladi-&-sarah.jpg'
+import vid7 from '/public/images/videos/Stephane.png'
 
 const videos = [
     {
         id: 1,
         persons: 'Lisa-Marie & Charles',
         date: '12.15.2023',
-        bgImg: vid1,
+        bgImg: vid5,
         description: 'Jura ist ein großartiger Karriereweg, wenn man ein breites Spektrum an Fähigkeiten erwerben möchte, das von kritischem Denken und strategischer Planung bis hin zur Kommunikation reicht. Wenn man gerne Herausforderungen annimmt.',
         link: 'https://www.youtube.com/embed/nKuE7ynd6X0?si=zXKl2TbfF2L6Ql9t',
         place: 'Allemagne'
@@ -53,16 +56,16 @@ const videos = [
         id: 6,
         persons: 'Sarah & Vladi',
         date: '08 12 2023',
-        bgImg: vid3,
+        bgImg: vid6,
         description: 'Jura ist ein großartiger Karriereweg, wenn man ein breites Spektrum an Fähigkeiten erwerben möchte, das von kritischem Denken und strategischer Planung bis hin zur Kommunikation reicht. Wenn man gerne Herausforderungen annimmt.',
         link: 'https://www.youtube.com/embed/XxfjT8NCkrU',
         place: 'Allemagne'
     },
     {
         id: 7,
-        persons: 'Sarah & Stephane',
+        persons: '',
         date: '08 12 2023',
-        bgImg: vid3,
+        bgImg: vid7,
         description: 'Jura ist ein großartiger Karriereweg, wenn man ein breites Spektrum an Fähigkeiten erwerben möchte, das von kritischem Denken und strategischer Planung bis hin zur Kommunikation reicht. Wenn man gerne Herausforderungen annimmt.',
         link: 'https://www.youtube.com/embed/rzEP767qF-E',
         place: 'Allemagne'
