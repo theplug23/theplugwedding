8cdbb94edff232bae22755e93c6ab3834e56fd36
theplug23 <travauxtheplug@gmail.com>
2024-03-11 10:04:39 +0100

    update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -15,11 +15,24 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"/>
           <link rel='icon' type='image/png' href='../images/LOGO WEDDING - BLANC.png' />
-          <meta name="google-site-verification" content="M8xKPKLGmWRheGfhhhVJcXhNgm-in72rcCs4vTa8hOU" />        
+          <meta name="google-site-verification" content="M8xKPKLGmWRheGfhhhVJcXhNgm-in72rcCs4vTa8hOU" />
+          <meta name="google-site-verification" content="googlece8be7bf29be3524.html" />        
         </Head>
         <body>
           <Main />
           <NextScript />
+          {/* Google Tag Manager script */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments)}
+                gtag('js', new Date());
+                gtag('config', 'G-B5PGZNWSDD');
+              `
+            }}
+          />
+          <script  async src="https://www.googletagmanager.com/gtag/js?id=G-B5PGZNWSDD"></script>
         </body>
       </Html>
     )
