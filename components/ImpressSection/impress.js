ee426af8e28a0fe633e01a64a9aabe33712fe332
theplug23 <travauxtheplug@gmail.com>
2024-03-28 18:10:22 +0100

    add impressum page

---
+++ components/ImpressSection/impress.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import SectionTitle from "../SectionTitle";
+
+const ImpressumSection = (props) => {
+    return (
+        <section className={`wpo-Service-section section-padding ${props.pbClass}`}>
+            <div className="container ff">
+                <SectionTitle MainTitle={'IMPRESSUM'} subTitle={'Impressum'} />
+                <div className="row">
+                    <div className="col-lg-12">
+                        <div className="impressum-content">
+                            <h1 style={{fontWeight: "bold", fontFamily:"Montserrat"}}>Impressum</h1><br/>
+                            <h4 style={{fontWeight: "bold", fontFamily:"Montserrat"}}>Gemäß § 5 TMG</h4>
+                            <p>Nova Gold</p>
+                            <p>Landwehrstr. 89</p>
+                            <p>64293 Darmstadt</p><br/>
+                            <h4 style={{fontWeight: "bold", fontFamily:"Montserrat"}}>Vertreten durch:</h4>
+                            <p>Frau Hasina Osmani</p><br/>
+                            <h4 style={{fontWeight: "bold", fontFamily:"Montserrat"}}>Kontakt</h4>
+                            <p>Telefon: 0175 / 8498497</p>
+                            <p>E-Mail: info@novaeventcenter.com</p><br/>
+                            <h4 style={{fontWeight: "bold", fontFamily:"Montserrat"}}>Umsatzsteuer-Identifikationsnummer</h4>
+                            <p>Umsatzsteuer-Identifikationsnummer gemäß § 27a Umsatzsteuergesetz: DE 290 519 654</p><br/>
+                            <h4 style={{fontWeight: "bold", fontFamily:"Montserrat"}}>Streitschlichtung in der EU</h4>
+                            <p>Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a href="https://ec.europa.eu/consumers/odr/">https://ec.europa.eu/consumers/odr/</a>.</p>
+                            <p>Die E-Mail-Adresse finden Sie oben im Impressum.</p><br/>
+                            <h4 style={{fontWeight: "bold", fontFamily:"Montserrat"}}>Verbraucherstreitbeilegung/Universalschlichtungsstelle</h4>
+                            <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.</p>
+                            <p>Quelle: eRecht24</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default ImpressumSection;
---
+++ pages/Impressum/index.js
@@ -0,0 +1,37 @@
+import React, { Fragment, useEffect } from 'react';
+import PageTitle from '../../components/pagetitle/PageTitle';
+import Navbar from '../../components/Navbar/Navbar';
+import ImpressumSection from '../../components/ImpressSection/impress';
+import Footer from '../../components/footer/Footer';
+import Scrollbar from '../../components/scrollbar/scrollbar';
+import { Helmet } from 'react-helmet';
+import ReactGA from 'react-ga4';
+
+const ImpressumPage = () => {
+    const ClickHandler = () => {
+        window.scrollTo(10, 0);
+    };
+
+    useEffect(() => {
+        ReactGA.event({
+            hitType: 'pageview',
+            page: window.location.pathname,
+            title: "Impressum PAGE"
+        });
+    }, []);
+
+    return (
+        <Fragment>
+            <Helmet>
+                <title>Impressum - THEPLUG WEDDING</title>
+            </Helmet>
+            <Navbar />
+            <PageTitle pageTitle={'Impressum'} pagesub={'Impressum'} /> 
+            <ImpressumSection pbClass={'pt-0'}/>
+            <Footer />
+            <Scrollbar />
+        </Fragment>
+    );
+};
+
+export default ImpressumPage;
