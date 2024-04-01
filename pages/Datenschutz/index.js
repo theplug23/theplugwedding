007f94eab909390b75a29095903be4733e9480b8
theplug23 <travauxtheplug@gmail.com>
2024-04-01 15:31:25 +0200

    ajout de la page des mentions legales

---
+++ pages/Datenschutz/index.js
@@ -0,0 +1,37 @@
+import React, { Fragment, useEffect } from 'react';
+import PageTitle from '../../components/pagetitle/PageTitle';
+import Navbar from '../../components/Navbar/Navbar';
+import DatenschutzSection from '../../components/DatenschutzSection/datenschutz';
+import Footer from '../../components/footer/Footer';
+import Scrollbar from '../../components/scrollbar/scrollbar';
+import { Helmet } from 'react-helmet';
+import ReactGA from 'react-ga4';
+
+const DatenschutzPage = () => {
+    const ClickHandler = () => {
+        window.scrollTo(10, 0);
+    };
+
+    useEffect(() => {
+        ReactGA.event({
+            hitType: 'pageview',
+            page: window.location.pathname,
+            title: "Datenschutz PAGE"
+        });
+    }, []);
+
+    return (
+        <Fragment>
+            <Helmet>
+                <title>Datenschutz - THEPLUG WEDDING</title>
+            </Helmet>
+            <Navbar />
+            <PageTitle pageTitle={'Datenschutz'} pagesub={'Datenschutz'} /> 
+            <DatenschutzSection pbClass={'pt-0'}/>
+            <Footer />
+            <Scrollbar />
+        </Fragment>
+    );
+};
+
+export default DatenschutzPage;
