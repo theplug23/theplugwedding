b36152b9cda1218cb7c41aef4f3e55ba2cb811dc
theplug23 <travauxtheplug@gmail.com>
2024-10-09 11:52:35 +0200

    anchor style on mobile

--- pages/Angebote/index.js
+++ pages/Angebote/index.js
@@ -98,7 +98,7 @@ const ShopPage = ({ addToCart, addToWishList }) => {
         <title>Angebote - THEPLUG WEDDING</title>
       </Helmet>
       <Navbar />
-      <div style={{textAlign: 'center', marginBottom: '20px', scrollBehavior: 'smooth', marginTop:"20px"}}>
+      <div className='ancre' style={{textAlign: 'center', scrollBehavior: 'smooth', marginTop:"20px"}}>
         <a href="#photo" style={{marginRight: '20px', backgroundColor: '#b99226', color:"white", padding:"10px 15px", borderRadius:"5px"}}>Angebote für Fotos</a>
         <a href="#video" style={{marginRight: '20px', backgroundColor: '#b99226', color:"white", padding:"10px 15px", borderRadius:"5px"}}>Angebote für Video</a>
         <a href="#photo-video" style={{backgroundColor: '#b99226', color:"white", padding:"10px 15px", borderRadius:"5px"}}>Angebote für Fotos & Video</a>
