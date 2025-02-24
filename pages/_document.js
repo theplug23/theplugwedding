fb7f34d249b1396cded6500519bde373e45c1ef9
theplug23 <travauxtheplug@gmail.com>
2025-02-24 15:38:29 +0100

    Tag added

--- pages/_document.js
+++ pages/_document.js
@@ -48,17 +48,26 @@ export default class MyDocument extends Document {
             content="Professioneller Hochzeitsfotograf und Videograf aus Darmstadt. Emotionale Hochzeitsfotos und Filme, die Ihre Liebe weltweit einfangen. Jetzt unverbindlich anfragen!"
           />
 
-          <script dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P46QXHZK');`}}></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-P46QXHZK');`,
+            }}
+          />
 
         </Head>
         <body>
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P46QXHZK"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-P46QXHZK"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
           {/* Google Tag Manager script */}
