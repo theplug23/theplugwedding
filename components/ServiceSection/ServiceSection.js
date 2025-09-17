1800af3f3569cd7eb4c574a9e338fb5ddd1849fb
theplug23 <travauxtheplug@gmail.com>
2025-09-17 10:23:43 +0100

    multiselection de service

--- components/ServiceSection/ServiceSection.js
+++ components/ServiceSection/ServiceSection.js
@@ -19,6 +19,7 @@ const ServiceSection = (props) => {
         props.onServiceSelect(service);
     }
     const { t } = useTranslation()
+    const { selectedServices } = props; // Destructure selectedServices from props
     return (
 
         <section className={`wpo-Service-section section-padding ${props.pbClass}`} id="Service">
@@ -29,25 +30,26 @@ const ServiceSection = (props) => {
                 <p>{t("Wir präsentieren euch unsere professionellen Hochzeitsfotografie-Dienstleistungen, die darauf abzielen, die unvergesslichen Momente eures besonderen Tages in wunderschönen Bildern festzuhalten. Mit einem kreativen und geschulten Auge fangen wir die Emotionen, die Schönheit und die einzigartigen Details ein, die eure Hochzeit so besonders machen. Ob romantische Porträts oder spontane Augenblicke – unsere Fotos erzählen eure Geschichte mit Stil und Eleganz. Vertraut auf unsere Erfahrung und Leidenschaft, um die Erinnerungen an euren schönsten Tag für die Ewigkeit festzuhalten.")}</p>
                 <div className="wpo-Service-wrap">
                     <div className="row">
-                        {Services.map((service, sitem) => (
-                            <div className="col-lg-3 col-md-6 col-sm-6 col-12" style={{ cursor: "pointer", }} key={sitem} onClick={() => handleClick(service)}>
-                                <div className="wpo-Service-item">
-                                    <div className="wpo-Service-img">
-                                        <Image src={service.img} alt="service" />
+                        {Services.map((service, sitem) => {
+                            const isSelected = selectedServices.some((s) => s.id === service.id);
+                            return (
+                                <div className="col-lg-3 col-md-6 col-sm-6 col-12" style={{ cursor: "pointer", }} key={sitem} onClick={() => handleClick(service)}>
+                                    <div className={`wpo-Service-item ${isSelected ? 'selected-service' : ''}`}>
+                                        <div className="wpo-Service-img">
+                                            <Image src={service.img} alt="service" />
+                                        </div>
+                                        <div className="wpo-Service-text text-wrap">
+                                            <h2 style={{ fontFamily: "Montserrat", fontSize: "22px", fontWeight: "600", marginTop: "10px" }} >{service.price} €</h2>
+                                        </div><ul className="list-item">
+                                            {service.packages.map((pack) => (
+                                                <li style={{ fontSize: '14px', color: "black" }} key={pack.id} className="item">{t(pack.slug)}</li>
+                                            ))}
+                                        </ul>
                                     </div>
-                                    <div className="wpo-Service-text text-wrap">
-                                        <h2 style={{ fontFamily: "Montserrat", fontSize: "22px", fontWeight: "600", marginTop: "10px" }} >{service.price} €</h2>
-                                    </div>
+
                                 </div>
-                                <div className="wpo-Service-item item-service">
-                                    <ul className="list-item">
-                                        {service.packages.map((pack) => (
-                                            <li style={{ fontSize: '14px', color: "black" }} key={pack.id} className="item">{t(pack.slug)}</li>
-                                        ))}
-                                    </ul>
-                                </div>
-                            </div>
-                        ))}
+                            )
+                        })}
 
                     </div>
                 </div>
@@ -56,25 +58,26 @@ const ServiceSection = (props) => {
                 <p>{t("Wir bieten euch unvergessliche Hochzeitsvideos, die eure einzigartigen Momente für immer festhalten. Mit unserer Leidenschaft für Film und einem kreativen Auge schaffen wir emotionale und authentische Videos, die eure Liebesgeschichte in den Mittelpunkt stellen. Von den kleinen Details bis hin zu den großen Augenblicken – wir fangen alles ein, damit ihr diese besonderen Momente immer wieder erleben könnt. Unser Ziel ist es, eure Hochzeit so festzuhalten, wie sie wirklich war: voller Emotionen, Freude und Liebe. Lasst uns gemeinsam euren schönsten Tag in einem Film verewigen, der euch ein Leben lang begleiten wird!")}</p>
                 <div className="wpo-Service-wrap">
                     <div className="row">
-                        {VServices.map((vservice, vsitem) => (
-                            <div className="col-lg-3 col-md-6 col-sm-6 col-12" key={vsitem} style={{ cursor: "pointer" }} onClick={() => handleClick(vservice)}>
-                                <div className="wpo-Service-item">
-                                    <div className="wpo-Service-img">
-                                        <Image src={vservice.img} alt="service" />
+                        {VServices.map((vservice, vsitem) => {
+                            const isSelected = selectedServices.some((s) => s.id === vservice.id);
+                            return (
+                                <div className="col-lg-3 col-md-6 col-sm-6 col-12" key={vsitem} style={{ cursor: "pointer" }} onClick={() => handleClick(vservice)}>
+                                    <div className={`wpo-Service-item ${isSelected ? 'selected-service' : ''}`}>
+                                        <div className="wpo-Service-img">
+                                            <Image src={vservice.img} alt="service" />
+                                        </div>
+                                        <div className="wpo-Service-text text-wrap">
+                                            <h2 style={{ fontFamily: "Montserrat", fontSize: "20px", fontWeight: "600", marginTop: "10px" }}>{vservice.price} €</h2>
+                                        </div> <ul className="list-item">
+                                            {vservice.packages.map((pack) => (
+                                                <li style={{ fontSize: '14px', color: "black" }} key={pack.id} className="item">{t(pack.slug)}</li>
+                                            ))}
+                                        </ul>
                                     </div>
-                                    <div className="wpo-Service-text text-wrap">
-                                        <h2 style={{ fontFamily: "Montserrat", fontSize: "20px", fontWeight: "600", marginTop: "10px" }}>{vservice.price} €</h2>
-                                    </div>
+
                                 </div>
-                                <div className="wpo-Service-item item-service">
-                                    <ul className="list-item">
-                                        {vservice.packages.map((pack) => (
-                                            <li style={{ fontSize: '14px', color: "black" }} key={pack.id} className="item">{t(pack.slug)}</li>
-                                        ))}
-                                    </ul>
-                                </div>
-                            </div>
-                        ))}
+                            )
+                        })}
                     </div>
                 </div>
                 <div id="photo-video" style={{ height: "280px" }}></div>
@@ -82,25 +85,26 @@ const ServiceSection = (props) => {
                 <p>{t("Wir bieten euch einzigartige Foto- und Videodienstleistungen für euren Hochzeitstag an, die all die besonderen Momente festhalten, die ihr für immer in Erinnerung behalten möchtet. Unser Team sorgt dafür, dass ihr euch an eurem großen Tag voll und ganz auf das Feiern konzentrieren könnt, während wir im Hintergrund die Magie in professionellen Bildern und emotionalen Videos einfangen. Mit Liebe zum Detail und einem Auge für authentische Augenblicke gestalten wir eure Hochzeitsgeschichte in einem individuellen Stil, der eure Persönlichkeit widerspiegelt. Lasst uns gemeinsam unvergessliche Erinnerungen schaffen, die ein Leben lang halten.")}</p>
                 <div className="wpo-Service-wrap">
                     <div className="row">
-                        {OServices.map((oservice, ositem) => (
-                            <div className="col-lg-3 col-md-6 col-sm-6 col-12" key={ositem} style={{ cursor: "pointer" }} onClick={() => handleClick(oservice)}>
-                                <div className="wpo-Service-item">
-                                    <div className="wpo-Service-img">
-                                        <Image src={oservice.img} alt="service" />
+                        {OServices.map((oservice, ositem) => {
+                            const isSelected = selectedServices.some((s) => s.id === oservice.id);
+                            return (
+                                <div className="col-lg-3 col-md-6 col-sm-6 col-12" key={ositem} style={{ cursor: "pointer" }} onClick={() => handleClick(oservice)}>
+                                    <div className={`wpo-Service-item ${isSelected ? 'selected-service' : ''}`}>
+                                        <div className="wpo-Service-img">
+                                            <Image src={oservice.img} alt="service" />
+                                        </div>
+                                        <div className="wpo-Service-text text-wrap">
+                                            <h2 style={{ fontFamily: "Montserrat", fontSize: "22px", fontWeight: "600", marginTop: "10px" }}>{oservice.price} €</h2>
+                                        </div> <ul className="list-item">
+                                            {oservice.packages.map((pack) => (
+                                                <li style={{ fontSize: '14px', color: "black" }} key={pack.id} className="item">{t(pack.slug)}</li>
+                                            ))}
+                                        </ul>
                                     </div>
-                                    <div className="wpo-Service-text text-wrap">
-                                        <h2 style={{ fontFamily: "Montserrat", fontSize: "22px", fontWeight: "600", marginTop: "10px" }}>{oservice.price} €</h2>
-                                    </div>
+
                                 </div>
-                                <div className="wpo-Service-item item-service">
-                                    <ul className="list-item">
-                                        {oservice.packages.map((pack) => (
-                                            <li style={{ fontSize: '14px', color: "black" }} key={pack.id} className="item">{t(pack.slug)}</li>
-                                        ))}
-                                    </ul>
-                                </div>
-                            </div>
-                        ))}
+                            )
+                        })}
                     </div>
                 </div>
             </div>
