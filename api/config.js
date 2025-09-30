a477a382bb8db53032ed02f3bdf9edaae5c7c5b1
theplug23 <travauxtheplug@gmail.com>
2025-09-30 04:47:33 +0100

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,7 +1,7 @@
 // api/config.js
 import axios from 'axios';
 
-const API_BASE_URL = 'https://www.fotograf-darmstdat.de/api'
+const API_BASE_URL = 'https://theplug-wedding.com/api'; // 'https://www.fotograf-darmstdat.de/api'
 // process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3008';
 
 // Instance axios avec configuration de base
