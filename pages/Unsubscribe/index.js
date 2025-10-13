ab54dc618ac5dc5d4a507a9074054e5726d3ed28
theplug23 <travauxtheplug@gmail.com>
2025-10-13 11:07:41 +0100

    configuration de la page unsubscribe

--- pages/Unsubscribe/index.js
+++ pages/Unsubscribe/index.js
@@ -1,31 +1,64 @@
-import React, { Fragment, useEffect } from 'react';
-import { Helmet } from 'react-helmet';
-import Navbar from '../../components/Navbar/Navbar';
-import Footer from '../../components/footer/Footer';
-import Scrollbar from '../../components/scrollbar/scrollbar';
-import UnsubscribeSection from '../../components/UnsubscribeSection/unsubscribe'
-import ReactGA from 'react-ga4';
+"use client";
+import React, { Fragment, useEffect } from "react";
+import { Helmet } from "react-helmet";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/footer/Footer";
+import Scrollbar from "../../components/scrollbar/scrollbar";
+import UnsubscribeSection from "../../components/UnsubscribeSection/unsubscribe";
+import ReactGA from "react-ga4";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/router";
 
 const UnsubscribePage = () => {
-    useEffect(() => {
-        ReactGA.event({
-            hitType: 'pageview',
-            page: window.location.pathname,
-            title: 'Unsubscribe PAGE'
-        });
-    }, []);
+  useEffect(() => {
+    ReactGA.event({
+      hitType: "pageview",
+      page: window.location.pathname,
+      title: "Unsubscribe PAGE",
+    });
+  }, []);
 
-    return (
-        <Fragment>
-            <Helmet>
-                <title>Unsubscribe - THEPLUG WEDDING</title>
-            </Helmet>
-            <Navbar />
-            <UnsubscribeSection />
-            <Footer />
-            <Scrollbar />
-        </Fragment>
-    );
+  const router = useRouter();
+  const { unsubscribeToken } = router.query;
+
+  // const searchParams = useSearchParams()
+  // const unSubscribeToken = searchParams.get('unsubscribeToken');
+  console.log("unsubscribeToken", unsubscribeToken);
+  const handleUnsubscribe = async () => {
+    try {
+        console.log('je suis ici dans unsubscribe');
+        
+      await fetch("https://theplug-wedding.com/api/newsletter/unsubscribe", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ unsubscribeToken }),
+      });
+    } catch (error) {
+      console.error("Error unsubscribing:", error);
+    }
+  };
+
+
+  useEffect(() => {
+    if (unsubscribeToken) {
+      handleUnsubscribe();
+      router.replace({ pathname: router.pathname, query: {} });
+    }
+  }, [unsubscribeToken]);
+
+  return (
+    <Fragment>
+      <Helmet>
+        <title>Unsubscribe - THEPLUG WEDDING</title>
+      </Helmet>
+      <Navbar />
+      <UnsubscribeSection />
+      <Footer />
+      <Scrollbar />
+    </Fragment>
+  );
 };
 
 export default UnsubscribePage;
