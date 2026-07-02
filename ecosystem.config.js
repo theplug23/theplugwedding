a5d588124f310f281d95e506948a7f335363f1c3
theplug23 <travauxtheplug@gmail.com>
2026-07-02 18:44:14 +0200

    ports problem resolved

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,22 +2,22 @@ module.exports = {
   apps : [ 
     { 
       name : "ThePlug Wedding", 
-      exec_mode: 'fork', 
-      instances: 'max', 
+      exec_mode: 'cluster', 
+      instances: '1', 
       script : "node_modules/next/dist/bin/next", 
       args: 'start', 
       watch: true, 
       env_local: { 
         APP_ENV: 'local',
-        PORT: 3009
+        PORT: 3030
       }, 
       env_development: { 
         APP_ENV: 'dev',
-        PORT: 3009
+        PORT: 3030
       }, 
       env_production: { 
         APP_ENV: 'prod',
-        PORT: 3009
+        PORT: 3030
       } 
     } 
   ] 
