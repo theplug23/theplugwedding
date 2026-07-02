0037d6c2b7416e3221261c64a250edcfb5b6f1b5
theplug23 <travauxtheplug@gmail.com>
2026-07-02 17:18:45 +0200

    production port changed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,24 @@
-module.exports = {
-  apps : [
-    {
-      name   : "ThePlug Wedding",
-      exec_mode: 'fork',
-      instances: 'max',
-      script : "node_modules/next/dist/bin/next",
-      args: 'start',
-      watch: true,
-      env_local: {
-        APP_ENV: 'local'
-      },
-      env_development: {
-        APP_ENV: 'dev'
-      },
-      env_production: {
-        APP_ENV: 'prod'
-      }
-    }
-  ]
+module.exports = { 
+  apps : [ 
+    { 
+      name : "ThePlug Wedding", 
+      exec_mode: 'fork', 
+      instances: 'max', 
+      script : "node_modules/next/dist/bin/next", 
+      args: 'start', 
+      watch: true, 
+      env_local: { 
+        APP_ENV: 'local',
+        PORT: 3009
+      }, 
+      env_development: { 
+        APP_ENV: 'dev',
+        PORT: 3009
+      }, 
+      env_production: { 
+        APP_ENV: 'prod',
+        PORT: 3009
+      } 
+    } 
+  ] 
 }
